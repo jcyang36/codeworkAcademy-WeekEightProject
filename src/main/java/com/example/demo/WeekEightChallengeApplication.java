031123d5b34203cad46d95456537a38a2ea1cd52
Joyce Yang <jcyang@hmc.edu>
2017-07-11 15:01:16 -0400

    Added email capability

--- src/main/java/com/example/demo/WeekEightChallengeApplication.java
+++ src/main/java/com/example/demo/WeekEightChallengeApplication.java
@@ -1,12 +1,16 @@
 package com.example.demo;
 
+import it.ozimov.springboot.mail.configuration.EnableEmailTools;
+import org.hibernate.validator.constraints.Email;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.thymeleaf.expression.Lists;
 
 @SpringBootApplication
+@EnableEmailTools
 public class WeekEightChallengeApplication {
-
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		SpringApplication.run(WeekEightChallengeApplication.class, args);
 	}
 }
